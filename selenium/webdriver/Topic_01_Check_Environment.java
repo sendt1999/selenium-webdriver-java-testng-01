f81b7bd66c6113a36dbab6b8efbaf2573968ef81
Do Sen <senmoon.1999@gmail.com>
2023-07-12 01:04:47 +0700

    123

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -16,7 +16,7 @@ public class Topic_01_Check_Environment {
 	String osName = System.getProperty("os.name");
 
 	@BeforeClass
-	
+	// 123456
 	public void beforeClass() {
 		if (osName.contains("Windows")) {
 			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
