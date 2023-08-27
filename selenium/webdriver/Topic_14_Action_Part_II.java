702cc0a326526224d326c5cfe487fefa8b3a4581
Do Sen <senmoon.1999@gmail.com>
2023-08-28 01:45:08 +0700

    Topic_14_Action_Part_II

--- selenium/webdriver/Topic_14_Action_Part_II.java
+++ selenium/webdriver/Topic_14_Action_Part_II.java
@@ -1,15 +1,22 @@
 package webdriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Topic_14_Action_Part_II {
 	WebDriver driver;
+	Actions action;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
 
@@ -23,31 +30,70 @@ public class Topic_14_Action_Part_II {
 		}
 
 		driver = new FirefoxDriver();
+		action = new Actions(driver);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		// driver.manage().window().maximize();
 		// driver.get("https://www.facebook.com/");
 	}
 
 	@Test
-	public void TC_01_() {
+	public void TC_01_Click_And_Hold_Block() {
+		driver.get("https://automationfc.github.io/jquery-selectable/");
+		
+		List<WebElement> listNumber = driver.findElements(By.cssSelector("ol#selectable>li"));
+		// Đang chứa 12 số/ item trong List này
+		
+		// 1 - Click vào số 1 (source) - 2 -> Vẫn giữ chuột chưa nhả ra
+		action.clickAndHold(listNumber.get(0))
+		// 3 - Di chuột tới số (target)
+		.moveToElement(listNumber.get(7))
+		
+		// 4 - Nhả chuột trái ra
+		.release()
+		
+		// Execute
+		.perform();
+		
+		sleepInSecond(3);
+		
+		List<WebElement> listSelectNumber = driver.findElements(By.cssSelector("ol#selectable>li.ui-selected"));
+		Assert.assertEquals(listSelectNumber.size(), 8);
 		
 	}
 
 	@Test
-	public void TC_02_() {
+	public void TC_02_Click_And_Hold_Random() {
+		driver.get("https://automationfc.github.io/jquery-selectable/");
 		
+		// Chạy được cho cả MAC/ Windows
+		Keys key = null;
+		if (osName.contains("Windows")) {
+			key = Keys.CONTROL;
+		} else {
+			key = Keys.COMMAND;
+		}
+		
+		List<WebElement> listNumber = driver.findElements(By.className("ol#selectable>li"));
+		// Đang chứa 12 số/ item trong List này
+		
+		// Nhấn Ctrl xuống
+		action.keyDown(key).perform();
+		
+		// Click chọn các số random
+		action.click(listNumber.get(0))
+		.click(listNumber.get(3))
+		.click(listNumber.get(7))
+		.click(listNumber.get(10)).perform();
+		
+		// Nhả phím Ctrl ra
+		action.keyUp(key).perform();
+		
+		sleepInSecond(5);
+		
+		List<WebElement> listSelectNumber = driver.findElements(By.cssSelector("ol#selectable>li.ui-selected"));
+		Assert.assertEquals(listSelectNumber.size(), 4);
 	}
 
-	@Test
-	public void TC_03_() {
-		
-	}
-	
-	/*
-	 * public void randomNumber() { Random rand = new Random(); rand.nextInt(99999);
-	 * }
-	 */
-	
 	public void sleepInSecond(long timeInSecond) {
 		try {
 			Thread.sleep(timeInSecond * 1000);
